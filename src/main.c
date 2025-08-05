ddbd0c9c62b6532f44dfa626bd5be54d935e39bf
jacobsmxth <jacob.d.smith@live.com>
2025-08-04 22:21:40 -0400

    setup the structs of the program

--- src/main.c
+++ src/main.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 
+
+
+// Storing money as cents
+
+typedef struct Income{
+  char *name;
+  long long money;
+  struct Income *next;
+} Income;
+
+typedef struct {
+  char *name;
+  long long money;
+  struct Expense *next;
+} Expense;
+
+typedef struct Budget {
+  char *name;
+  long long current;
+  long long max;
+  struct Budget *next;
+} Budget;
+
+typedef struct {
+  char* name;
+  char* passHash;
+  Income *incomes;
+  Expense *expenses;
+  Budget *budgets;
+} User;
+
 int main(void) {
 
+
+  printf("Finance App\n");
   return 0;
 }
