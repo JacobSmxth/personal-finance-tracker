8d4cd49ae466e3df99c5be3cd0bff8128d3915cb
jacobsmxth <jacob.d.smith@live.com>
2025-08-04 22:28:40 -0400

    Setup the createUser function

--- src/main.c
+++ src/main.c
@@ -1,4 +1,9 @@
+#define _POSIX_C_SOURCE 200809L
+
+
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 
 
@@ -31,7 +36,28 @@ typedef struct {
   Budget *budgets;
 } User;
 
+
+User *createUser(char *name, char *password) {
+  User *newUser = calloc(1, sizeof(*newUser));
+  if (!newUser) {
+    perror("failed to create new user");
+    return NULL;
+  }
+  newUser->name = strdup(name);
+  newUser->passHash = strdup(password);
+  newUser->incomes = NULL;
+  newUser->expenses = NULL;
+  newUser->budgets = NULL;
+
+  return newUser;
+}
+
+
+
 int main(void) {
+  User *myUser = createUser("Test", "testPass");
+  printf("%s\n", myUser->name);
+
 
 
   printf("Finance App\n");
